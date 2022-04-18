96e4d685efcab500d5592ff0ec4a2822ff9ef6e5
AntonBaltser <antonbaltser@yahoo.com>
2022-04-19 00:42:30 +0300

    neues Thema "constructor"

--- main/src/oop_basics/Main.java
+++ main/src/oop_basics/Main.java
@@ -2,11 +2,14 @@ package oop_basics;
 
 public class Main {
     public static void main (String[] args) {
-        Person gochacha = new Person();
-        gochacha.height = 120;
+        Person gochacha = new Person(120, "Gochacha");
+//        gochacha.height = 120;
         gochacha.say("Pet9");
-        System.out.println(gochacha.height);
+        System.out.println(gochacha.name + "\'s height is " + gochacha.height);
         Person vlad = new Person();
         System.out.println(vlad.height);
+
+        Person dima = new Person("Dima");
+        System.out.println(dima.name);
     }
 }
