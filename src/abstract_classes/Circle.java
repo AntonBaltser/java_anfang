19eb2a90d1b0a2748130b2083e52d911145f354a
AntonBaltser <antonbaltser@yahoo.com>
2022-04-19 14:09:30 +0300

    Абстрактные классы

---
+++ src/abstract_classes/Circle.java
@@ -0,0 +1,7 @@
+package abstract_classes;
+
+public class Circle extends Shape{
+    public void draw () {
+        System.out.print("Круг");
+    }
+}
---
+++ src/abstract_classes/Main.java
@@ -0,0 +1,8 @@
+package abstract_classes;
+
+public class Main {
+    public static void main (String[] args) {
+        Shape circle = new Circle();
+        circle.draw();
+    }
+}
